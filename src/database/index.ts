7d09e4a7670336500d22c79cdf04b4162392b0df
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-04-09 15:31:24 -0300

    Update Added new table to the Database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,14 @@
 import { QuickDB } from "quick.db";
 import { MemberBloods } from "./interfaces/MemberBloods.js";
 import { ChannelBloodsIgnored } from "./interfaces/ChannelBloodsIgnored.js";
+import { MemberBloodsRank } from "./interfaces/MemberBloodsRank.js";
 
 const filePath = rootTo("localdatabase.sqlite");
 
 const database = {
     memberBloods: new QuickDB<MemberBloods>({ filePath, table: "MemberBloods" }),
     channelBloodsIgnored: new QuickDB<ChannelBloodsIgnored>({ filePath, table: "ChannelBloodsIgnored" }),
+    memberBloodsRank: new QuickDB<MemberBloodsRank>({ filePath, table: "MemberBloodsRank" }),
 };
 
 export { database };
---
+++ src/database/interfaces/MemberBloodsRank.ts
@@ -0,0 +1,5 @@
+export interface MemberBloodsRank {
+    userId?: string[];
+    userBloods?: number[];
+    userRank?: number[];
+}
