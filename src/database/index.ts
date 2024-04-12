63d30dbb5091911ced5ddf62b6b2e58844476552
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-04-12 20:05:02 -0300

    Update Added new table in Database

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,11 +3,13 @@ import { MemberBloods } from "./interfaces/MemberBloods.js";
 import { ChannelBloodsIgnored } from "./interfaces/ChannelBloodsIgnored.js";
 import { MemberBloodsRank } from "./interfaces/MemberBloodsRank.js";
 import { ActiveMemberDuration } from "./interfaces/ActiveMemberDuration.js";
+import { MemberBloodsLost } from "./interfaces/MemberBloodsLost.js";
 
 const filePath = rootTo("localdatabase.sqlite");
 
 const database = {
     memberBloods: new QuickDB<MemberBloods>({ filePath, table: "MemberBloods" }),
+    memberBloodsLost: new QuickDB<MemberBloodsLost>({ filePath, table: "MemberBloodsLost" }),
     channelBloodsIgnored: new QuickDB<ChannelBloodsIgnored>({ filePath, table: "ChannelBloodsIgnored" }),
     memberBloodsRank: new QuickDB<MemberBloodsRank>({ filePath, table: "MemberBloodsRank" }),
     activeMemberDuration: new QuickDB<ActiveMemberDuration>({ filePath, table: "ActiveMemberDuration" }),
---
+++ src/database/interfaces/MemberBloodsLost.ts
@@ -0,0 +1,3 @@
+export interface MemberBloodsLost {
+    bloodsLost?: number;
+}
