6f993047b284f260fb4066e0803ac8a01e07d37f
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-05-10 00:06:04 -0300

    Update: Removido o subcomando carteira e adicionado cooldown de 5 minuto no comando de Bloods

--- src/discord/commands/Bloods/Wallet.ts
+++ src/discord/commands/Bloods/Wallet.ts
@@ -2,26 +2,25 @@ import { Command } from "#base";
 import { CommandTimer } from "#classes";
 import { database } from "#database";
 import { bloodsWalletMenu } from "#menus";
-import { ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
+import { ApplicationCommandType, time } from "discord.js";
 
 new Command({
     name: "bloods",
-    description: "Sistema de Bloods",
+    description: "Veja o seu saldo de Bloods, loja e ranks",
     dmPermission: false,
     type: ApplicationCommandType.ChatInput,
-    options: [
-        {
-            name: "carteira",
-            description: "Veja o seu saldo de Bloods, opção de transferencia, loja e ranks",
-            type: ApplicationCommandOptionType.Subcommand,
-        },
-    ],
     async run(interaction) {
         const userId = interaction.user.id;
 
-        const test = new CommandTimer(userId, "Test");
-        
-        test.setTimer();
+        const commandCooldown = new CommandTimer(userId, "Wallet");
+
+        commandCooldown.setTimer(300);
+
+        if (await commandCooldown.verifyTimer())
+            return await interaction.reply({
+                content: `Você pode usar esse comando novamente ${time(await commandCooldown.getTimer(), "R")}`,
+                ephemeral: true,
+            });
 
         const sendCommandsChannel = "1113654401786183783"; //? ID do canal que o comando poderá ser enviado
 
@@ -32,32 +31,18 @@ new Command({
                 ephemeral: true,
             });
 
-        const getSubcommand = interaction.options.getSubcommand();
-        switch (getSubcommand) {
-            case "carteira": {
-                const userId = interaction.user.id;
+        const userName = interaction.user.displayName;
+        const userIcon = interaction.user.avatarURL();
+        const userBloods = await database.memberBloods.get(`${userId}.bloods`);
+        const GetUserRank = await database.memberBloodsRank.get<any[]>("MembersRank");
+        let userRank = null;
 
-                const userName = interaction.user.displayName;
-
-                const userIcon = interaction.user.avatarURL();
-
-                const userBloods = await database.memberBloods.get(`${userId}.bloods`);
-
-                const GetUserRank = await database.memberBloodsRank.get<any[]>("MembersRank");
-
-                let userRank = null;
-
-                GetUserRank?.forEach((element) => {
-                    if (element.userId === userId) {
-                        userRank = element.userRank;
-                    }
-                });
-
-                await interaction.reply(bloodsWalletMenu(userId, userName, userIcon, userBloods, userRank, GetUserRank?.length));
-
-                break;
+        GetUserRank?.forEach((element) => {
+            if (element.userId === userId) {
+                userRank = element.userRank;
             }
-        }
-        return;
+        });
+
+        return await interaction.reply(bloodsWalletMenu(userId, userName, userIcon, userBloods, userRank, GetUserRank?.length));
     },
 });
