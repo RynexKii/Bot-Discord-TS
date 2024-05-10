6a36521919b8edaa46c3854bf262e941e06c80df
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-05-10 00:06:38 -0300

    Feat

---
+++ src/discord/commands/Staff/resetCooldowns.ts
@@ -0,0 +1,15 @@
+import { Command } from "#base";
+import { database } from "#database";
+import { ApplicationCommandType } from "discord.js";
+
+new Command({
+    name: "reset",
+    description: "Resetar todos os cooldowns dos comandos da Nea",
+    dmPermission: false,
+    type: ApplicationCommandType.ChatInput,
+    async run(interaction) {
+        await database.commandTimer.deleteAll();
+
+        return await interaction.reply({ content: "Todos os cooldowns dos comandos da Nea foram reiniciados com sucesso.", ephemeral: true });
+    },
+});
