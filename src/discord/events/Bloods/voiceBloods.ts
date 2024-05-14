81c4ac7c8f35ac12b0e826b70fd5b81accc46f29
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-05-14 18:35:35 -0300

    Update: Agora quando existir apenas um usuário no voice chat ele ganhara menos Bloods

--- src/discord/events/Bloods/voiceBloods.ts
+++ src/discord/events/Bloods/voiceBloods.ts
@@ -8,47 +8,59 @@ new Event({
     async run(client) {
         async function addBloodsVoiceChannel() {
             const allMembersGuild: string[] = [];
-
-            const getGuildIdDB = await database.channelBloodsIgnored.get<string[]>("GuildConfig.guildId");
-
-            const getAllChannelsDB = await database.channelBloodsIgnored.get<string[]>("GuildConfig.allChannels");
+            const guildIdDB = await database.channelBloodsIgnored.get<string[]>("GuildConfig.guildId");
+            const allChannelsDB = await database.channelBloodsIgnored.get<string[]>("GuildConfig.allChannels");
 
             // Verifica se existe um guildId registrado no Banco de Dados
-            if (!getGuildIdDB) return;
+            if (!guildIdDB) return;
 
             // De todas as Guilds que o bot está, pega a guild que está registrada no Banco de Dados
-            const guild = client.guilds.cache.get(getGuildIdDB[0]);
+            const guild = client.guilds.cache.get(guildIdDB[0]);
 
             // Verifica se existe um valor na variável guild
             if (!guild) return;
 
-            // Pega todos os ID's dos membros da guild e armazena no Array allMembersGuild
-            const res = await guild.members.fetch();
-            res.forEach((member) => {
+            // Pega todos os ID's dos usuários da guild e armazena no Array allMembersGuild
+            const membersGuild = await guild.members.fetch();
+            membersGuild.forEach((member) => {
                 allMembersGuild.push(member.user.id);
             });
 
-            for (let index = 0; index < allMembersGuild.length; index++) {
+            for (const index in allMembersGuild) {
                 const membersId = allMembersGuild[index];
 
-                // Passando por todos os membros ele pega os ID's dos canais que tem membros conectados ( Também pega os que não estão conectado returnando null )
-                const getMemberVoiceId = (await guild.members.fetch(membersId)).voice.channelId;
+                // Passando por todos os usuários ele pega os ID's dos canais que tem usuários conectados ( Também pega os que não estão conectado returnando null )
+                const memberVoiceConnectedId = (await guild.members.fetch(membersId)).voice.channelId;
 
-                // Verifica se o getMemberVoiceId não existe um ID fazendo o return
-                if (getMemberVoiceId) {
-                    // Passa o ID do canal para pagar se o membro está conectado no canal do Servidor Principal
-                    const memberHasConnected = guild.channels.cache.has(getMemberVoiceId);
+                if (memberVoiceConnectedId) {
+                    // Passa o ID do canal para pagar se o usuário está conectado no canal do Servidor Principal
+                    const memberHasConnected = guild.channels.cache.has(memberVoiceConnectedId);
 
-                    // Uma segunda verificação se o membro está conectado no canal do Servidor Principal
+                    // Uma segunda verificação se o usuário está conectado no canal do Servidor Principal
                     if (memberHasConnected) {
-                        // Verificação dos canais que não concederão Bloods
-                        if (getAllChannelsDB) {
-                            if (!getAllChannelsDB.includes(getMemberVoiceId)) {
-                                // Verifica se o usuário tem Boost ativo no servidor para dar 1.5x de Bloods
-                                if (guild.members.cache.get(membersId)?.premiumSince) {
-                                    await database.memberBloods.add(`${membersId}.bloods`, randomNumber(1, 3));
+                        // Verifica se existe um canal registrado no Banco de Dados para ser ignorado
+                        if (allChannelsDB) {
+                            // Verificação dos canais que não concederão Bloods
+                            if (!allChannelsDB.includes(memberVoiceConnectedId)) {
+                                // Verifica se o usuário deu boost no Servidor
+                                const memberHasBoosted = guild.members.cache.get(membersId)?.premiumSince;
+
+                                // Retorna quantos usuário tem no canal
+                                const membersVoiceSize = (await guild.members.fetch(membersId)).voice.channel?.members.size;
+
+                                if (membersVoiceSize === 1) {
+                                    if (memberHasBoosted) {
+                                        await database.memberBloods.add(`${membersId}.bloods`, randomNumber(1, 2));
+                                    } else {
+                                        await database.memberBloods.add(`${membersId}.bloods`, 1);
+                                    }
                                 } else {
-                                    await database.memberBloods.add(`${membersId}.bloods`, randomNumber(1, 2));
+                                    // Verifica se o usuário tem Boost ativo no servidor para dar 1.5x de Bloods
+                                    if (memberHasBoosted) {
+                                        await database.memberBloods.add(`${membersId}.bloods`, randomNumber(1, 3));
+                                    } else {
+                                        await database.memberBloods.add(`${membersId}.bloods`, randomNumber(1, 2));
+                                    }
                                 }
                             }
                         }
@@ -59,6 +71,6 @@ new Event({
 
         setInterval(() => {
             addBloodsVoiceChannel();
-        }, 60 * 1000);
+        }, 120 * 1000);
     },
 });
